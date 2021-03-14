ed867bb102c0fe2a8762ba801c76b6f4761baa9a
Yanhao Li <yl4735@columbia.edu>
2021-03-14 11:54:29 +0000

    updated with create controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 describe PostsController do
   #reference: https://everydayrails.com/2012/04/07/testing-series-rspec-controllers.html
   PostsController.new
-  it "create a new post in database" do
-    Post.destroy_all
+  it "successfully complete creation" do   
+    User.destroy_all
+    test1 = FactoryBot.create(:user,
+        email: "728977862@qq.com", 
+        password: "4156GOGOGO", 
+        created_at: "2021-03-13 11:04:06",
+        updated_at: "2021-03-13 11:04:06"
+    )
     expect { 
-      post :create, post: FactoryBot.attributes_for(:post) 
-    }.to change(Post, :count).by(1) 
+      post :create, params: {post: FactoryBot.attributes_for(:post)}
+    }.to change(Post, :count).by(1) and redirect_to root_path
   end
-  
 end
