858332f127e89ed3c069ba8e83bd4e72ced5bb95
xixixixxixixixi <728977862@qq.com>
2021-04-05 03:23:12 +0800

    git push

--- db/migrate/20210311063328_create_posts.rb
+++ db/migrate/20210311063328_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :description
       t.integer :user_id
+      t.string :visibility
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_163257) do
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.integer "user_id"
+    t.string "visibility"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
